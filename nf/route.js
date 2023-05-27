51d0c3706854549c597215a41ef9174312b85fa6
dxuian <deathxunnat@gmail.com>
2023-05-28 02:01:41 +0530

    finxing communcation algo rn

--- nf/route.js
+++ nf/route.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29d4a44ab6c8e9f167a2912313135985b0e5e5e09ef0b549ae4342bb9866eb46
-size 33581
+oid sha256:483f71279b6814d202148e370deac1f9b003c97fafd97bdcf43adb0ddaf87cd9
+size 34219
