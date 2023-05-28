89828e865bf489257ecc6ebed0925fd5b407ae44
dxuian <deathxunnat@gmail.com>
2023-05-28 07:56:34 +0530

    fixed wpmacc calc now gkeep+beautify+speed

--- nf/public/prac.js
+++ nf/public/prac.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39678ae93369f6feb9808ac7bee471a2115542c83786e7287e508d0c3ee47ed3
-size 14579
+oid sha256:76637d51a8e90efcc47cfcd19a0c219214c3bb3a49ad22abc3f7ac7485d2a4a8
+size 14676
--- nf/route.js
+++ nf/route.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:483f71279b6814d202148e370deac1f9b003c97fafd97bdcf43adb0ddaf87cd9
-size 34219
+oid sha256:a471a3e469446536b1bf6c87ec45dd127db8471fa0817562b1e3c1c1531abc0e
+size 35375
