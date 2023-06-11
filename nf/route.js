70713e2ac844d05736c0e9761a69002887b59ad7
dxuian <deathxunnat@gmail.com>
2023-06-11 12:43:23 +0530

    b4 rm nodemon

--- nf/route.js
+++ nf/route.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68c366abf05ed9d03c49f6cb98a038f7165c92564604bfebbb5dcfd4d0fc67d2
-size 55150
+oid sha256:ddc49c8a4d6388c8240c0153fc4721f8fba180cc7e3caf7bc85974e6f085bfe5
+size 55166
