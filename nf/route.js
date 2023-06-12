95821b492cd329b896bd68bb4acf2e46a167c0f4
dxuian <deathxunnat@gmail.com>
2023-06-12 18:50:36 +0530

    del netlify cuz id doesnt host nodejs server

--- nf/route.js
+++ nf/route.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83839898dde754cae77693287d19921e5d14eabcd0a1a6e3bfdfe9718054ec40
-size 55129
+oid sha256:7026b981fa8ae544625eb4d237adde1123c3ec8810416b2bd3bea2b8cecf349a
+size 55146
