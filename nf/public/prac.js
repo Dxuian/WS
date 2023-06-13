e58eea600a7b6f7e898b4e1cffddd48782180f90
dxuian <deathxunnat@gmail.com>
2023-06-13 13:57:21 +0530

    for msazure

--- nf/public/prac.js
+++ nf/public/prac.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37aa856cd7e211b6252f7399ef1e883e33871b2d085eb7269042e827f93126ae
-size 18585
+oid sha256:13e924971813c1d9c919292f404e26c38df0e1baff7d09bcfbf7cae542660e6e
+size 18699
