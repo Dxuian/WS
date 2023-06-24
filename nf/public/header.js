520fdd36fbd55f2d98ea726b6d9e37d88efd2f43
dxuian <deathxunnat@gmail.com>
2023-06-24 11:35:42 +0530

    fix the postion of the globalwinners ;

--- nf/public/header.js
+++ nf/public/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10c913a3a8ab9e7af45038d9ce0c77ece6215e6d724506cd6773eaf0e245d33e
-size 1878
+oid sha256:d05eec5494186f17e75a01cc6a577a9a93452dce9e0a09043236265fdbd6ebed
+size 1921
