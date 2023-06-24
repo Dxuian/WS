2423fc44226fdd3ceb84236e2ed42e1126dba240
dxuian <deathxunnat@gmail.com>
2023-06-25 02:49:17 +0530

    fixed multibtn bug

--- nf/public/colap.js
+++ nf/public/colap.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:824a2323d41c27dd896cee230be4ac7613fa11115cbfd34889652d4283be66f0
-size 5744
+oid sha256:5ece36218d463b90f10b2470929e296fa9184b5c4244c1f060456c8f9dcecbfd
+size 5778
