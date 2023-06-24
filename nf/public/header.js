d3d22b72dfef342337bdb556689c2c1fca80cd4d
dxuian <deathxunnat@gmail.com>
2023-06-24 16:56:19 +0530

    fixed globalbtn

--- nf/public/header.js
+++ nf/public/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d05eec5494186f17e75a01cc6a577a9a93452dce9e0a09043236265fdbd6ebed
-size 1921
+oid sha256:4caca96e68b686a3e45a8b8688affd28fef79f7605f97a3ba169ee264633ee83
+size 1940
