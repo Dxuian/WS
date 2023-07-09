b00c42ba06b0eae19ddae2b8fcd957b789327a29
dxuian <deathxunnat@gmail.com>
2023-07-09 16:56:17 +0530

    az links changed

--- nf/public/prac.js
+++ nf/public/prac.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f73d66c4391b6c59e09eedfcb4d92c00bbbf5dd0f10de16343e13abd03bb333d
-size 19323
+oid sha256:fb28d298390b8cccca060e643f43de507e52a315853bb6e5ff832ec91217ab10
+size 19325
