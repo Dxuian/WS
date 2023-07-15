3c28e62a01a983810be7061feb1cef4526f4fac0
dxuian <deathxunnat@gmail.com>
2023-07-15 21:46:52 +0530

    rm data.js

--- nf/a.js
+++ nf/a.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83696b12aca384b6d08f4d30c9378bb4bb8d455bfa8078f24af838db1f9d78ef
-size 5938
+oid sha256:3f1dc2c5f041c82930dfc5dd0cc67c2e6797e4dfafa2327af534d39521138b27
+size 5941
